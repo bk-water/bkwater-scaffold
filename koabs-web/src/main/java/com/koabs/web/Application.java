bd0a59e5cb2004f1fd1c7ca285df6b9d49012c66
koabs <koabs@foxmail.com>
2019-01-31 18:28:18 +0800

    Spring boot init

--- koabs-web/src/main/java/com/koabs/web/Application.java
+++ koabs-web/src/main/java/com/koabs/web/Application.java
@@ -1,14 +1,32 @@
 package com.koabs.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.task.TaskSchedulingAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class Application {
+@SpringBootApplication(exclude = {JacksonAutoConfiguration.class, TaskSchedulingAutoConfiguration.class})
+@Slf4j
+public class Application extends SpringBootServletInitializer {
+
+	/**
+	 *   切换成 war 打包模式时
+	 *   需排除 pom 中spring-boot-starter-tomcat 依赖 改成 provided 方式
+	 * @param builder
+	 * @return
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		log.info("====== Tomcat 外部服务器启动加载成功 ======");
+		return builder.sources(Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-
+		log.info("====== APP Jar 服务启动成功 ======");
 	}
 
 }
